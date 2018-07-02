d7ed099a5e4307ba998b7188ff3555924def4672
patrick blume <PatrickBlume@gmx.de>
2018-07-02 14:17:39 +0200

    Added new testing file

---
+++ MzIdentMLDB_Library/MaxPlankTest.fsx
@@ -0,0 +1,32 @@
+﻿    
+#r "System.ComponentModel.DataAnnotations.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\netstandard.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\\BioFSharp.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\\BioFSharp.IO.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\Microsoft.EntityFrameworkCore.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\Microsoft.EntityFrameworkCore.Relational.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\Microsoft.EntityFrameworkCore.Sqlite.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\Remotion.Linq.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\SQLitePCLRaw.core.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\SQLitePCLRaw.batteries_v2.dll"
+#r @"..\packages\FSharp.Data.2.4.6\lib\net45\FSharp.Data.dll"
+#r @"..\packages\FSharp.Data.Xsd.1.0.2\lib\net45\FSharp.Data.Xsd.dll"
+#r "System.Xml.Linq.dll"
+#r @"..\MzIdentMLDB_Library\bin\Debug\MzIdentMLDB_Library.dll"
+
+open System
+open System.Data
+open System.Linq
+open System.Collections.Generic
+open FSharp.Data
+open Microsoft.EntityFrameworkCore
+open MzIdentMLDataBase.DataModel
+open MzIdentMLDataBase.InsertStatements.ObjectHandlers
+//open MzIdentMLDataBase.XMLParsing
+
+
+let fileDir = __SOURCE_DIRECTORY__
+let standardDBPathSQLite = fileDir + "\Databases\Test.db"
+
+let sqliteContext = ContextHandler.sqliteConnection standardDBPathSQLite
+
